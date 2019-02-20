05f99390102a4e3e4a730cc635c9b3a4ca2d432d
leianne louis <leilouis112@gmail.com>
2019-02-20 16:57:51 -0500

    completed get by id cohorts

--- routes/cohortRoutes.js
+++ routes/cohortRoutes.js
@@ -4,6 +4,13 @@ const knexConfig = require('../knexfile');
 const router = express();
 const db = knex(knexConfig.development)
 
+const checkNewCohort = (req, res, next) => {
+    if(req.body.name) {
+        next();
+    } else {
+        res.status(404).json('Please add a name for the cohort!')
+    }
+}
 router.get('/', async (req, res) => {
     try{
         const cohorts = await db('cohorts');
@@ -17,5 +24,34 @@ router.get('/', async (req, res) => {
         console.log(error)
     }
 })
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    try{
+        const cohort = await db('cohorts').where({id}).first()
+        console.log(cohort)
+        if(cohort){
+            res.status(200).json(cohort)
+        } else {
+            res.status(404).json({message: "we cant find that cohort"})
+        }
+    }
+    catch(error){
+        res.status(500).json({message: 'We are working on fixing this issue!'})
+    }
+})
+router.post('/', checkNewCohort, async (req, res) => {
+    const cohort = req.body;
+    try{
+        const newCohort = await db('cohorts').insert(cohort);
+        res.status(201).json({message: "cohort created!"})
+    }
+    catch(error){
+        res.status(500).json({message: 'We are working on fixing this issue!'})
+    }
+})
+
+
 
 module.exports = router;
